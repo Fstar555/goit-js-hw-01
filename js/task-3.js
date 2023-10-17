b9dbdf2b10f520416cdf87c05341ce481f336bab
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2023-10-17 12:40:56 +0300

    update index

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,11 @@
 "use strict";
 
-
 function getElementWidth(content, padding, border) {
-    return
-    (Number.parseInt(content) +
+  return;
+  Number.parseInt(content) +
     Number.parseInt(padding) * 2 +
-    Number.parseInt(padding) * 2;)
+    Number.parseInt(padding) * 2;
 }
-
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
